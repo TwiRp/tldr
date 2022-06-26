7436bcac04b33ea727bc2c2354a61c7827054140
kmwallio <mwallio@gmail.com>
2022-06-26 16:37:10 -0700

    Change back to 3 sentences

--- public/js/hashnode.js
+++ public/js/hashnode.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
         const body = await response.json();
         const post = body.data.post;
         const text = post.contentMarkdown;
-        const sentences = 5;
+        const sentences = 3;
         console.log(post.title);
         $.ajax({
             url: "/api/summarize",
